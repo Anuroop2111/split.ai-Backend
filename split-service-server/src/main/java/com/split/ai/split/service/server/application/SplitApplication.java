25b3c75137665a4e424cd7a560c566da880ff4be
Anuroop <anuroopls2001@gmail.com>
2025-09-11 23:05:29 +0530

    adding repo scan

--- split-service-server/src/main/java/com/split/ai/split/service/server/application/SplitApplication.java
+++ split-service-server/src/main/java/com/split/ai/split/service/server/application/SplitApplication.java
@@ -3,8 +3,10 @@ package com.split.ai.split.service.server.application;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "com.split.ai")
+@EntityScan(basePackages = "com.split.ai.split.service.repository.entity")
 @OpenAPIDefinition
 public class SplitApplication {
     public static void main(String[] args) {
